74b2c0d1db59fbca95f388c444c4e66367a3b62c
Martin Feckie <mfeckie@gmail.com>
2020-02-19 22:28:09 +1100

    Add uuid generation

--- rust-uuid/src/lib.rs
+++ rust-uuid/src/lib.rs
@@ -1,19 +1,15 @@
+extern crate uuid;
 mod utils;
+use uuid::Uuid;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, rust-uuid!");
+pub fn gen_v4() -> String {
+  let v4 = Uuid::new_v4();
+  v4.to_hyphenated().to_string()
 }

2e4e833947c68f340a6f56a07805e8fef82b599f
Martin Feckie <mfeckie@gmail.com>
2020-02-21 07:57:49 +1100

    Simplify mandelbrot invocation

--- app/components/mandelbrot/component.ts
+++ app/components/mandelbrot/component.ts
@@ -1,16 +1,8 @@
 import Component from "@glimmer/component";
 
-interface MandelbrotArgs {}
-
-export default class Mandelbrot extends Component<MandelbrotArgs> {
-  canvas?: HTMLCanvasElement;
-  constructor(owner: any, args: MandelbrotArgs) {
-    super(owner, args);
-    this.initMandelbrot();
-  }
-
-  async initMandelbrot() {
+export default class Mandelbrot extends Component {
+  async initMandelbrot(canvas: HTMLCanvasElement) {
     const { Chart } = await import("rust-plotter");
-    Chart.mandelbrot(this.canvas);
+    Chart.mandelbrot(canvas);
   }
 }
